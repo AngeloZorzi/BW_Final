132593ea25fd1b962212ff2c26b57fe47aef956c
AngeloZorzi <astro.drg666@gmail.com>
2025-06-23 16:55:28 +0200

    upd

--- src/main/java/it/epicode/BW_Final/config/SecurityConfig.java
+++ src/main/java/it/epicode/BW_Final/config/SecurityConfig.java
@@ -47,12 +47,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> cors.configure(http)) 
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/import/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 );
 
