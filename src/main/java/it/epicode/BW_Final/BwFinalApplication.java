8c61387095b9ab32d79cca3ce663400eb1de82ed
AngeloZorzi <astro.drg666@gmail.com>
2025-06-23 11:48:56 +0200

    Initial commit

---
+++ src/main/java/it/epicode/BW_Final/BwFinalApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.BW_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/BW_Final/BwFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.BW_Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
