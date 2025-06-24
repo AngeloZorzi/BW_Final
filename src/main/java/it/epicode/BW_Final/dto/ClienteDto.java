dc001c922c8f059225451a894b1a28340809c117
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-06-24 10:37:23 +0200

    Updated annotations

--- src/main/java/it/epicode/BW_Final/dto/ClienteDto.java
+++ src/main/java/it/epicode/BW_Final/dto/ClienteDto.java
@@ -2,9 +2,7 @@ package it.epicode.BW_Final.dto;
 
 
 import it.epicode.BW_Final.enumeration.TipoCliente;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -14,34 +12,52 @@ import java.time.LocalDate;
 public class ClienteDto {
 
     @NotEmpty
+    @NotBlank(message = "La ragione sociale è obbligatoria")
     private String ragioneSociale;
+
     @NotEmpty
+    @NotBlank(message = "La partita IVA è obbligatoria")
     private String partitaIva;
-    @NotNull
+
+    @NotNull(message = "La data di inserimento è obbligatoria")
     private LocalDate dataInserimento;
-    @NotNull
+
+    @NotNull(message = "La data dell'ultimo contatto è obbligatoria")
     private LocalDate dataUltimoContatto;
-        @NotNull
+
+    @NotNull(message = "Il fatturato è obbligatorio")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Il fatturato deve essere positivo")
     private BigDecimal fatturatoAnnuale;
+
     @Email
     private String pec;
+
     @NotEmpty
+    @NotBlank(message = "Il numero di telefono è obbligatorio")
     private String telefono;
-    @Email
+
+    @Email(message = "Email non valida")
     private String emailContatto;
+
     @NotEmpty
     private String nomeContatto;
+
     @NotEmpty
     private String cognomeContatto;
+
     @NotEmpty
     private String telefonoContatto;
+
     @NotEmpty
     private String logoAziendale;
+
     @NotEmpty
     private TipoCliente tipoCliente;
-    @NotNull
+
+    @NotNull(message = "L'indirizzo della sede legale è obbligatorio")
     private IndirizzoDto sedeLegale;
-    @NotNull
+
+    @NotNull(message = "L'indirizzo della sede operativa è obbligatorio")
     private IndirizzoDto sedeOperativa;
 
 }
